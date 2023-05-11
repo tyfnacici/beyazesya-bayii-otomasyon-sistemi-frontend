92fec2fee142bb5c74b17bfdd808442192378bf4
elif-ozkan <94003321+elif-ozkan@users.noreply.github.com>
2023-05-11 15:01:23 +0300

    fix(login): fix login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Login from "./pages/Login.jsx";
 const App = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      {/* <Login /> */}
+      <Login />
     </div>
   );
 };
